f0c85b025637c9d107003b2b3b904753bf7c97b4
hamdanabod55-collab <hamdanabod55@gmail.com>
2026-05-02 20:57:55 +0300

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,13 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="ar" dir="rtl">
       <head>
-        {
-  "whitelisted_domains":[
-    "<WHITELISTED_DOMAIN>",
-    "<WHITELISTED_DOMAIN>",
-    ...
-  ]
-}
+       <meta name="facebook-domain-verification" content="ljylgttmkeim9crvco5voes0vpr5hf" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Tajawal:wght@300;400;500;700&family=Cairo:wght@400;600;700&display=swap" rel="stylesheet" />
